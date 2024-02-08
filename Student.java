c1932130236c7963b77f91b3fedf6d1e20a96198
Peter Ryan <pryan24@students.polytechnic.org>
2024-02-08 14:38:58 -0800

    Added a bunch of documents with the fields for out project

---
+++ Student.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Student {
+    
+
+
+    //fields
+
+    private String name;
+    private int idNo;
+    private static int idGen;
+    private double balance;
+    ArrayList<Transaction> charges = new ArrayList<Transaction>();
+
+
+    //constructors
+
+    public Student(String myName, int myIdNo, double myBalance){
+        
+    }
+}
---
+++ Transaction.java
@@ -0,0 +1,17 @@
+public class Transaction{
+
+
+
+    //fields
+    private int month;
+    private int day;
+    private double charge;
+
+    //constructors
+
+    public Transaction(int myMonth, int myDay, double myCharge){
+        month = myMonth;
+        day = myDay;
+        charge = myCharge;
+    }
+}
